8f1349d62dc5f5a7d69741258c7bbef05b2296a4
alexsevv <aaa123qqq@mail.ru>
2017-05-03 10:50:39 +0300

    sugar

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,15 +25,15 @@ class User < ApplicationRecord
   before_validation { |user| user.username = user.username.downcase }
 
   def encrypt_password
-  	if self.password.present?
-	  	#создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
-	  	self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+    if self.password.present?
+	  #создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
+	  self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
-	  	#создаем хэш пароля - длинная уникальная строка, из которой невозможно восстановить
-	  	#исходный пароль
-	  	self.password_hash = User.hash_to_string(
-	  		OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-	  		)
+	  #создаем хэш пароля - длинная уникальная строка, из которой невозможно восстановить
+	  #исходный пароль
+	  self.password_hash = User.hash_to_string(
+	  	OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+	  	)
 	end
   end
 
@@ -51,8 +51,6 @@ class User < ApplicationRecord
   		user
   	else
   		nil
-  	end
+    end
   end
-
-
 end
