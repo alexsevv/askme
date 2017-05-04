c8766c99f26e3d333ef4171315c86b6d423ef65c
alexsevv <aaa123qqq@mail.ru>
2017-05-04 13:49:30 +0300

    add helper sklonenie and add block number-of-questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,29 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    ostatok = number % 100
+
+    if ostatok >= 11 && ostatok <= 14
+      return krokodilov
+    end
+
+    ostatok = number % 10
+
+    if ostatok == 1
+      return krokodil
+    end
+    if ostatok >= 2 && ostatok <= 4
+      return krokodila
+    end
+    if (ostatok >= 5 && ostatok <= 9) || ostatok == 0
+      return krokodilov
+    end
+  end
+
 end
