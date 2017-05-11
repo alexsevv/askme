c2429d95fa33b5aa9461e36080a317fb52d67740
alexsevv <aaa123qqq@mail.ru>
2017-05-11 14:27:54 +0300

    bgcolor header js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+function change_color(obj)
+{
+  obj.value && (document.getElementById("colorjs").style.backgroundColor = obj.value);
+}
