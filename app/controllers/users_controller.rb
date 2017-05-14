0f58877296a8929d705316579c92362b6a47ecaa
alexsevv <aaa123qqq@mail.ru>
2017-05-14 14:00:26 +0300

    edit color header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   def create
     redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
