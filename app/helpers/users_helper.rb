d75e24530616f43e55ee4191b33e5b687255a2e3
alexsevv <aaa123qqq@mail.ru>
2017-05-15 13:25:36 +0300

    sugar if else - ? : in user_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,10 @@
 module UsersHelper
   def user_avatar(user)
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      asset_path 'avatar.jpg'
-    end
+    user.avatar_url.present? ? user.avatar_url : asset_path('avatar.jpg')
   end
 
   def user_name(user)
-    if user.name.present?
-      user.name
-    else
-      content_tag 'span', '', class: "fa fa-user-secret", title: "не указано имя"
-    end
+    user.name.present? ? user.name :
+      content_tag('span', '', class: "fa fa-user-secret", title: "не указано имя")
   end
 end
