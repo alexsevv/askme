0f919e2da8ad53ca70cddd30cb7177191913944f
alexsevv <aaa123qqq@mail.ru>
2017-05-20 10:23:03 +0300

    sugar

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,35 +1,7 @@
 class Question < ApplicationRecord
-
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
-
-  # демонстрация жизненного цикла объкта
-  # навесили на все популярные коллбэки свои методы
-  #before_validation :before_validation
-  #after_validation :after_validation
-  #before_save :before_save
-  #before_create :before_create
-  #after_create :after_create
-  #after_save :after_save
-
-  #before_update :before_update
-  #after_update :after_update
-
-  #before_destroy :before_destroy
-  #after_destroy :after_destroy
-
-  #private
-
-  # динамически сгенерируем пару методов для каждого действия
-  # используя возможности руби
-  #['validation', 'save', 'create', 'update', 'destroy'].each do |action|
-    #['before', 'after'].each do |time|
-      #define_method("#{time}_#{action}") do
-        #puts "======> #{time}_#{action}"
-      #end
-    #end
-  #end
 end
